de1d143322578a294a948ca11df4ddf5f47e069b
DanPeled <danpeled31@gmail.com>
2023-09-14 16:24:07 +0300

    style changes, more measures

--- Scripts/JS/index.js
+++ Scripts/JS/index.js
@@ -2,44 +2,30 @@ let resultText = document.getElementById("resultText")
 let calcButton = document.getElementById("calculateButton")
 let textArea = document.getElementById("textarea")
 let dropdown = document.getElementById("dropdown")
-function calc(height, type) {
-    let func = calcBamba
-    let suffix = "Bamba"
-    switch (type) {
-        case ("snail"):
-            func = calcSnail
-            break
-        case ("water bear"):
-            func = calcWaterBear
-            break
-        case ("axolotl"):
-            func = calcAxolotl
-            break
-        case ("star nosed mole"):
-            func = calcStarNosedMole
-            break
-        case ("lowland streaked ternec"):
-            func = calcLowlandStreakedTernec
-            break
-        case ("dumbo octopus"):
-            func = calcDumboOctopus
-            break
-        case ("pink fairy armadillo"):
-            func = calcPinkFairyArmadillo
-            break
-        case ("red lipped batfish"):
-            func = calcRedLippedBatfish
-            break
-        case ("hedgehog"):
-            func = calcHedgehog
-            break
-    }
-    suffix = toTitleCase(type)
-    if (func(height) != 1) {
-        suffix += "s"
-    }
 
-    resultText.innerText = func(height).toFixed(2) + " " + suffix
+const calculationFunctions = {
+    "snail": calcSnail,
+    "water bear": calcWaterBear,
+    "axolotl": calcAxolotl,
+    "star nosed mole": calcStarNosedMole,
+    "lowland streaked ternec": calcLowlandStreakedTernec,
+    "dumbo octopus": calcDumboOctopus,
+    "pink fairy armadillo": calcPinkFairyArmadillo,
+    "red lipped batfish": calcRedLippedBatfish,
+    "hedgehog": calcHedgehog,
+    "three year-old": calcThreeYearOld,
+    "blobfish": calcBlobfish,
+    "tomato": calcTomato
+}
+function calc(height, type) {
+    const func = calculationFunctions[type] || calcBamba
+    const suffix = toTitleCase(type)
+
+    if (func(height) !== 1) {
+        resultText.innerText = `${func(height).toFixed(2)} ${suffix}s`
+    } else {
+        resultText.innerText = `${func(height).toFixed(2)} ${suffix}`
+    }
 }
 function calcBamba(height) {
     return height / 0.03
@@ -80,7 +66,15 @@ function calcRedLippedBatfish(height) {
 function calcHedgehog(height) {
     return height / 0.3
 }
-
+function calcThreeYearOld(height) {
+    return height / 1.016
+}
+function calcBlobfish(height) {
+    return height / 0.3
+}
+function calcTomato(height){
+    return height / 0.07
+}
 // Restricts input for the given textbox to the given inputFilter function.
 function setInputFilter(textbox, inputFilter, errMsg) {
     ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop", "focusout"].forEach(function (event) {
@@ -88,33 +82,33 @@ function setInputFilter(textbox, inputFilter, errMsg) {
             if (inputFilter(this.value)) {
                 // Accepted value.
                 if (["keydown", "mousedown", "focusout"].indexOf(e.type) >= 0) {
-                    this.classList.remove("input-error");
-                    this.setCustomValidity("");
+                    this.classList.remove("input-error")
+                    this.setCustomValidity("")
                 }
 
-                this.oldValue = this.value;
-                this.oldSelectionStart = this.selectionStart;
-                this.oldSelectionEnd = this.selectionEnd;
+                this.oldValue = this.value
+                this.oldSelectionStart = this.selectionStart
+                this.oldSelectionEnd = this.selectionEnd
             }
             else if (this.hasOwnProperty("oldValue")) {
                 // Rejected value: restore the previous one.
-                this.classList.add("input-error");
-                this.setCustomValidity(errMsg);
-                this.reportValidity();
-                this.value = this.oldValue;
-                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+                this.classList.add("input-error")
+                this.setCustomValidity(errMsg)
+                this.reportValidity()
+                this.value = this.oldValue
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd)
             }
             else {
                 // Rejected value: nothing to restore.
-                this.value = "";
+                this.value = ""
             }
-        });
-    });
+        })
+    })
 }
 
 setInputFilter(textArea, function (value) {
-    return /^\d*\.?\d*$/.test(value); // Allow digits and '.' only
-}, "Only digits and '.' are allowed");
+    return /^\d*\.?\d*$/.test(value) // Allow digits and '.' only
+}, "Only digits and '.' are allowed")
 
 calcButton.addEventListener("click", function () {
     calc(textArea.value, dropdown.value)
@@ -123,7 +117,29 @@ calcButton.addEventListener("click", function () {
 function toTitleCase(str) {
     if (str != undefined) {
         return str.toLowerCase().split(' ').map(function (word) {
-            return (word.charAt(0).toUpperCase() + word.slice(1));
-        }).join(' ');
+            return (word.charAt(0).toUpperCase() + word.slice(1))
+        }).join(' ')
     }
 }
+
+var options = Array.from(dropdown.options)
+options.sort(function (a, b) {
+    return a.text.localeCompare(b.text)
+})
+dropdown.innerHTML = ""
+options.forEach(function (option) {
+    dropdown.appendChild(option)
+})
+
+// Sort the table rows alphabetically (excluding the header)
+var table = document.querySelector("table")
+var rows = Array.from(table.querySelectorAll("tr"))
+rows.shift() // Remove the header row
+rows.sort(function (a, b) {
+    var measurementA = a.cells[0].textContent.toLowerCase()
+    var measurementB = b.cells[0].textContent.toLowerCase()
+    return measurementA.localeCompare(measurementB)
+})
+rows.forEach(function (row) {
+    table.appendChild(row)
+})
