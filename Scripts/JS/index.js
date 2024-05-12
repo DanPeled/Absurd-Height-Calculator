3c5b52ee4bb87078046a3f75779c51a4480c88d1
DanPeled <danpeled31@gmail.com>
2024-05-12 21:00:53 +0300

    made the measure map better

--- Scripts/JS/index.js
+++ Scripts/JS/index.js
@@ -1,115 +1,92 @@
-let resultText = document.getElementById("resultText")
-let calcButton = document.getElementById("calculateButton")
-let textArea = document.getElementById("textarea")
-let measurementDropdown = document.getElementById("measurementDropdown")
-let measurementTypeDropdown = document.getElementById("measure-type")
+let resultText = document.getElementById("resultText");
+let calcButton = document.getElementById("calculateButton");
+let textArea = document.getElementById("textarea");
+let measurementDropdown = document.getElementById("measurementDropdown");
+let measurementTypeDropdown = document.getElementById("measure-type");
 
+const calculationMeasures = {
+    "snail": { weight: 0.003028, height: 0.045 },
+    "water bear": { weight: 0.00001, height: 0.0012 },
+    "axolotl": { weight: 2.5, height: 0.3 },
+    "star nosed mole": { weight: 0.049, height: 0.019 },
+    "lowland streaked ternec": { weight: 2.25, height: 0.14 },
+    "dumbo octopus": { weight: 5.9, height: 0.25 },
+    "pink fairy armadillo": { weight: 0.120, height: 0.115 },
+    "red lipped batfish": { weight: 0.9, height: 0.4 },
+    "hedgehog": { weight: 0.8, height: 0.3 },
+    "three year-old": { weight: 13.5, height: 1.016 },
+    "blobfish": { weight: 9, height: 0.3 },
+    "tomato": { weight: 0.455, height: 0.07 },
+    "bamba": { weight: 0.00189, height: 0.03 },
+    "stanley even-heat pot": { weight: 1.238307, height: 0.213106 }
+};
 
-const calculationHeightMeasures = {
-    "snail": 0.045,
-    "water bear": 0.0012,
-    "axolotl": 0.3,
-    "star nosed mole": 0.019,
-    "lowland streaked ternec": 0.14,
-    "dumbo octopus": 0.25,
-    "pink fairy armadillo": 0.115,
-    "red lipped batfish": 0.4,
-    "hedgehog": 0.3,
-    "three year-old": 1.016,
-    "blobfish": 0.3,
-    "tomato": 0.07,
-    "bamba": 0.03,
-    "stanley even-heat pot": 0.213106
-}
-const calculationWeightMeasures = {
-    "snail": 0.003028,
-    "water bear": 0.00001,
-    "axolotl": 2.5,
-    "star nosed mole": 0.049,
-    "lowland streaked ternec": 2.25,
-    "dumbo octopus": 5.9,
-    "pink fairy armadillo": 0.120,
-    "red lipped batfish": 0.9,
-    "hedgehog": 0.8,
-    "three year-old": 13.5,
-    "blobfish": 9,
-    "tomato": 0.455,
-    "bamba": 0.00189,
-    "stanley even-heat pot": 1.238307
-}
-
-function calc(height, measure, measureType) {
-    const measurement = (measureType == "height") ?
-        calculationHeightMeasures[measure] : calculationWeightMeasures[measure];
+function calc(value, measure, measureType) {
+    const { weight, height } = calculationMeasures[measure];
 
     const suffix = toTitleCase(measure);
-    const newHeight = height / measurement;
-    if (newHeight !== 1) {
-        resultText.innerText = `${newHeight.toFixed(2)} ${suffix}s`
+    const newValue = (measureType === "height") ? value / height : value / weight;
+
+    if (newValue !== 1) {
+        resultText.innerText = `${newValue.toFixed(2)} ${suffix}s`;
     } else {
-        resultText.innerText = `${newHeight.toFixed(2)} ${suffix}`
+        resultText.innerText = `${newValue.toFixed(2)} ${suffix}`;
     }
 }
 
-// Restricts input for the given textbox to the given inputFilter function.
 function setInputFilter(textbox, inputFilter, errMsg) {
     ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop", "focusout"].forEach(function (event) {
         textbox.addEventListener(event, function (e) {
             if (inputFilter(this.value)) {
-                // Accepted value.
                 if (["keydown", "mousedown", "focusout"].indexOf(e.type) >= 0) {
-                    this.classList.remove("input-error")
-                    this.setCustomValidity("")
+                    this.classList.remove("input-error");
+                    this.setCustomValidity("");
                 }
 
-                this.oldValue = this.value
-                this.oldSelectionStart = this.selectionStart
-                this.oldSelectionEnd = this.selectionEnd
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                this.classList.add("input-error");
+                this.setCustomValidity(errMsg);
+                this.reportValidity();
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            } else {
+                this.value = "";
             }
-            else if (this.hasOwnProperty("oldValue")) {
-                // Rejected value: restore the previous one.
-                this.classList.add("input-error")
-                this.setCustomValidity(errMsg)
-                this.reportValidity()
-                this.value = this.oldValue
-                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd)
-            }
-            else {
-                // Rejected value: nothing to restore.
-                this.value = ""
-            }
-        })
-    })
+        });
+    });
 }
 
 setInputFilter(textArea, function (value) {
-    return /^\d*\.?\d*$/.test(value) // Allow digits and '.' only
-}, "Only digits and '.' are allowed")
+    return /^\d*\.?\d*$/.test(value);
+}, "Only digits and '.' are allowed");
 
 calcButton.addEventListener("click", function () {
-    calc(textArea.value, measurementDropdown.value, measurementTypeDropdown.value)
-})
+    calc(parseFloat(textArea.value), measurementDropdown.value, measurementTypeDropdown.value);
+});
 
 function toTitleCase(str) {
     if (str != undefined) {
         return str.toLowerCase().split(' ').map(function (word) {
-            return (word.charAt(0).toUpperCase() + word.slice(1))
-        }).join(' ')
+            return (word.charAt(0).toUpperCase() + word.slice(1));
+        }).join(' ');
     }
 }
 
-var options = Array.from(measurementDropdown.options)
+var options = Array.from(measurementDropdown.options);
 options.sort(function (a, b) {
-    return a.text.localeCompare(b.text)
-})
-measurementDropdown.innerHTML = ""
+    return a.text.localeCompare(b.text);
+});
+measurementDropdown.innerHTML = "";
 options.forEach(function (option) {
-    measurementDropdown.appendChild(option)
-})
+    measurementDropdown.appendChild(option);
+});
 
 function sortTable(table) {
     var rows = Array.from(table.querySelectorAll("tr"));
-    rows.shift(); // Remove the header row
+    rows.shift();
     rows.sort(function (a, b) {
         var measurementA = a.cells[0].textContent.toLowerCase();
         var measurementB = b.cells[0].textContent.toLowerCase();
